057bd8b2f8a5c237197119d6d8da8aa5b03db574
imoohb123 <144774100+imoohb123@users.noreply.github.com>
2023-09-12 16:04:11 +0530

    Add files via upload
    java codes

---
+++ Arithmatic.java
@@ -0,0 +1,16 @@
+public class Arithmatic
+{
+public static void main(String args[])
+{
+int a=88,b=90;
+int add,sub,mul,div;
+add=a+b;
+sub=a-b;
+mul=a*b;
+div=a/b;
+System.out.println("Addition ="+add);
+System.out.println("Subtraction ="+sub);
+System.out.println("Multiplicationtion ="+mul);
+System.out.println("Division ="+div);
+}
+}
